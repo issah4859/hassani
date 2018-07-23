9c0cbc5e5e051243cfdcba5dcb3cfe129b5cd97a
issa <you@example.com>
2018-07-23 21:59:25 +0300

    product modified

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -20,8 +20,14 @@ class ProductResource extends Resource
 
             'descriptions' => $this->detail,
             'price'=>$this->price,
-            'stock'=> $this->stock,
-            'discount' => $this->discount
+            'stock'=> $this->stock==0 ? 'Out of stock' : $this->stock,
+            'totalPrice' =>round ((1-$this->discount/100) * $this->price, 2),
+            'rating'=> $this->reviews->count() >0 ?round($this->reviews->sum('star')/$this->reviews->count(), 2): 'No reting yet',
+            'href' =>[
+
+                'reviews'=> route('reviews.index', $this->id)
+
+            ]
 
         ];
     }
